2243029f57a7bd7ee89053c4cb70fa897b57f5b9
tamimio <tamimio.yvo@hotmail.com>
2017-11-25 21:37:46 +0300

    works somehow

--- lab_6 (2) - semaphor/main.cpp
+++ lab_6 (2) - semaphor/main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 #include <windows.h>
-#include <conio.h>
-
-using namespace std;
 
 class CS
 {
@@ -11,38 +8,53 @@ public:
 	CS() { InitializeCriticalSection(&cs); }
 	void Enter() { EnterCriticalSection(&cs); }
 	void Leave() { LeaveCriticalSection(&cs); }
+	void Delete() { DeleteCriticalSection(&cs); }
 };
 
 class Event
 {
 	HANDLE hEvent;
 public:
-	Event();
-	~Event();
-	void Set();
+	Event() { CreateEvent(NULL, TRUE, FALSE, NULL); };
+	~Event() { Close(); }
+
+	void Set() { SetEvent(hEvent); }
 	void Reset();
-	DWORD Wait (int ms);
-	DWORD Wait () { Wait(INFINITE); };
+
+	/*DWORD*/ void Wait (int ms) { WaitForSingleObject(hEvent, ms); };
+	/*DWORD*/ void Wait () { Wait(INFINITE); };
+
+	void Close() { CloseHandle(hEvent); };
 };
 
-class Semaphor
+class Semaphore
 {
 	int maxCount;
 	int Count;
 	CS countCS;
 	Event ev;
 public:
-	Semaphor(int _maxCount) : maxCount(_maxCount), Count (0) {}
-	~Semaphor(){};
+	Semaphore(int _maxCount) : maxCount(_maxCount), Count (0)
+	{		
+		countCS=CS();
+		ev=Event();
+	}
+	~Semaphore()
+	{
+		countCS.Delete();
+		ev.Close();		
+	}
 
 	void Enter()
 	{
 		countCS.Enter();
 		if(Count++<maxCount)
 		{
+			std::cout << "Enter: CS is locked, total number of threads: " << Count << std::endl;
 			countCS.Leave();
 			return;
 		}
+		std::cout << "Enter: CS is unlocked, total number of threads: " << Count << std::endl;
 		countCS.Leave();
 		ev.Wait();
 	}
@@ -50,10 +62,46 @@ public:
 	void Leave()
 	{
 		countCS.Enter();
-		if(--Count>=maxCount)
+		if ((--Count)!=0) // if(--Count>=maxCount)
 		{
 			ev.Set();
+			std::cout << "Leave: CS is unlocked, total number of threads: " << Count << std::endl;
 		}
 		countCS.Leave();
 	}
 };
+
+Semaphore *sem;
+
+DWORD WINAPI foo(PVOID)
+{
+	sem->Enter();
+	Sleep(1000);
+	sem->Leave();
+	return 0;
+}
+
+int main()
+{	
+	int maxCount, num;
+	std::cout << "Input maxCount for Semaphore -> ";
+	std::cin >> maxCount;
+	std::cout << "Input number of threads -> ";
+	std::cin >> num;
+
+	sem = new Semaphore(maxCount);
+	HANDLE * thread = new HANDLE [num];
+	
+	for(int i=0; i<num; ++i)
+		thread[i]=CreateThread(NULL, 0, foo, NULL, 0, NULL);
+	
+	WaitForMultipleObjects(num, thread, TRUE, INFINITE);
+	
+	for(int i=0; i<num; ++i)
+		CloseHandle(thread[i]);
+	
+	std::cout<<"Done ";
+	return 0;
+}
+
+
